ca4538ea77f99438de7448faf83bc6bdc7cdb6ca
panManfredini <pan.manfredini@gmail.com>
2018-11-30 23:15:21 +0100

    button

--- src/button.js
+++ src/button.js
@@ -1,9 +1,17 @@
 import {brick as html} from "../node_modules/brick-element/build/brick.js"
-import {btn_css} from "./btn.js"
+import {btn_css} from "./css/btn.js"
 
 let mxn = html` 
 ${btn_css}
-<div class="btn"> ciao ciao</div>
+<div ${"#:btn"}> <slot>Click Me</slot></div>
     `;
 
-customElements.define("btn-x",class extends mxn(HTMLElement){});
+customElements.define("btn-x",class extends mxn(HTMLElement){
+    constructor(){
+        super();
+        this.addEventListener("click",this._onclick);
+    }
+    _onclick(){ 
+        if(typeof(this["onclick"]) === "function") this.onclick(); 
+    }
+});
---
+++ src/css/configs.js
@@ -0,0 +1,6 @@
+
+
+export let colorA='#00d1b2';
+export let colorA_d='#00c4a7';
+export let colorA_dd='#00b89c';
+export let colorA_dis='#00d1b2';
